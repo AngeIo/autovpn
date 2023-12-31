15faee0aecb754994246983422e6a6aa9e79be9e
Angelo GERARD <angelo.gerard@canal-plus.com>
2023-12-31 04:30:58 +0100

    Fixed potential issues with .key file because "WorkingDirectory" wasn't set for the startup shortcut

--- utils/make.py
+++ utils/make.py
@@ -31,15 +31,12 @@ def get_reg(name,path):
 # Remove autovpn.exe in the current directory
 if os.path.exists(parent_dir_file_path(program_name + '.exe')):
     os.remove(parent_dir_file_path(program_name + '.exe'))
-
 # Add your pyinstaller command here
 os.system('pyinstaller --onefile --noconsole --icon "' + parent_dir_file_path('assets/icon.ico') + '" "' + parent_dir_file_path(program_name + '.py') + '"')
-
 # Check if autovpn.exe exists in the dist folder
 if os.path.exists(parent_dir_file_path('dist/' + program_name + '.exe')):
     # Move autovpn.exe from dist to the current folder
     shutil.move(parent_dir_file_path('dist/' + program_name + '.exe'), parent_dir)
-
     # Check if dist folder is empty
     if not os.listdir(parent_dir_file_path('dist')):
         # Remove the dist folder
@@ -52,13 +49,10 @@ else:
 # --- Shortcut creation to current user desktop ---
 # Target of shortcut
 target = parent_dir_file_path(program_name + '.exe')
-
 # Name of link file
 linkName = program_name + '.lnk'
-
 # Read location of Windows desktop folder from registry
 desktopFolder = os.path.normpath(get_reg('Desktop', r'Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders'))
-
 # Path to location of link file
 pathLink = os.path.join(desktopFolder, linkName)
 shell = Dispatch('WScript.Shell')
@@ -71,16 +65,13 @@ shortcut.save()
 # --- Shortcut creation to user taskbar (pin) ---
 program_name_pttb = 'pttb.exe'
 program_path_pttb = shutil.which(program_name_pttb)
-
 if program_path_pttb is None:
     # The program is not installed, download it
     url = 'https://github.com/0x546F6D/pttb_-_Pin_To_TaskBar/releases/download/230124/pttb.exe'
     response = requests.get(url, allow_redirects=True)
-
     # Save the program to the current directory
     with open(program_name_pttb, 'wb') as f:
         f.write(response.content)
-
     print(f"{program_name_pttb} downloaded.")
 else:
     print(f"{program_name_pttb} is already installed.")
@@ -93,9 +84,9 @@ icon = parent_dir_file_path('assets/icon.ico')
 shell = Dispatch('WScript.Shell')
 shortcut = shell.CreateShortcut(pathLink)
 shortcut.TargetPath = target
+shortcut.WorkingDirectory = parent_dir
 shortcut.IconLocation = icon
 shortcut.save()
-
 # Run the program and pin it to the taskbar
 command = [program_name_pttb, target]
 subprocess.run(command, shell=True)
@@ -104,18 +95,14 @@ subprocess.run(command, shell=True)
 # Remove .spec files
 for spec_file in glob.glob(parent_dir_file_path('**/*.spec'), recursive=True):
     os.remove(spec_file)
-
 # Remove build folder
 if os.path.exists(parent_dir_file_path('build')):
     shutil.rmtree(parent_dir_file_path('build'))
-
 # Remove __pycache__ folders
 for pycache in glob.glob(parent_dir_file_path('**/__pycache__'), recursive=True):
     shutil.rmtree(pycache)
-
 flag_autostart = False
 answer = None
-
 # Ask user if they want to add the script to Windows startup process
 MessageBox = ctypes.windll.user32.MessageBoxW
 # If the script is already in the Windows startup process, skip this step
@@ -126,18 +113,15 @@ if os.path.exists(startup_pathLink):
     flag_autostart = True
 if flag_autostart == False:
     answer = MessageBox(None, "Do you want to add the script to the Windows' startup process so it opens each time you log in?", program_name + ' - Autostart?', 0x4 | 0x30 | 0x1000) # Respectively: MB_YESNO | MB_ICONWARNING | MB_SYSTEMMODAL
-# Details about MessageBoxW:
-# https://learn.microsoft.com/fr-fr/windows/win32/api/winuser/nf-winuser-messageboxw
-# https://stackoverflow.com/questions/2963263/how-can-i-create-a-simple-message-box-in-python
-# https://stackoverflow.com/questions/76020424/how-to-display-the-blue-warning-message-box-of-windows-in-python
-# https://stackoverflow.com/questions/27257018/python-messagebox-with-icons-using-ctypes-and-windll
 if answer == 6 or flag_autostart == True:
     # Add script to Windows startup process
     icon = parent_dir_file_path('assets/icon.ico')
     shortcut_startup = shell.CreateShortcut(startup_pathLink)
     shortcut_startup.TargetPath = target
+    shortcut_startup.WorkingDirectory = parent_dir
     shortcut_startup.IconLocation = icon
     shortcut_startup.save()
     if answer == 6:
         print('The script has been added to the Windows startup process!\n\nNext time you will restart your computer or log out and log in, the script will open automatically!')
+
 sys.exit(0)
